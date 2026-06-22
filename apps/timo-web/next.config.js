de614850d86a35e9e43bd04b8f064b4231157898
kimminna <kmina121777@naver.com>
2026-06-22 17:15:42 +0900

    file(ui): packages/ui를 timo-design-system으로 이름 변경 (#1)
    - packages/ui 설정 파일을 삭제하고 packages/timo-design-system으로 이전했습니다
    - 패키지명을 @repo/ui에서 @repo/timo-design-system으로 변경했습니다
    - apps/timo-web/next.config.js의 transpilePackages를 @repo/timo-design-system으로 업데이트했습니다

--- apps/timo-web/next.config.js
+++ apps/timo-web/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@repo/ui"],
+  transpilePackages: ["@repo/timo-design-system"],
 };
 
 export default nextConfig;
