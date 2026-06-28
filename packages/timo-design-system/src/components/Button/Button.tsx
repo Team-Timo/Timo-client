25b84ff141e8c0f2b44d874e09e184d669780782
kimminna <kmina121777@naver.com>
2026-06-28 15:48:14 +0900

    remove(ui): 샘플 Button 컴포넌트 및 스토리 제거 (#42)
    - 초기 플레이스홀더로 추가된 샘플 Button 컴포넌트를 제거했습니다

--- packages/timo-design-system/src/components/Button/Button.tsx
+++
@@ -1,7 +0,0 @@
-import type { ButtonHTMLAttributes } from "react";
-
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
-
-export const Button = ({ children, ...props }: ButtonProps) => {
-  return <button {...props}>{children}</button>;
-};
