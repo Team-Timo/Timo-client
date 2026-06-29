3d6ef63f6393af2282a138feb496b5dd58c67433
kimminna <kmina121777@naver.com>
2026-06-30 00:29:52 +0900

    fix(ui): Tag 컴포넌트 내부 고정된 width 속성 제거 및 px 속성 추가 (#48)

--- packages/timo-design-system/src/components/tag/Tag.tsx
+++ packages/timo-design-system/src/components/tag/Tag.tsx
@@ -2,9 +2,9 @@ export interface TagProps {
   text: string;
 }
 
-export const Tag = ({ text = "과제" }: TagProps) => {
+export const Tag = ({ text }: TagProps) => {
   return (
-    <div className="bg-timo-gray-300 flex h-4 w-7.5 items-center justify-center rounded-[4px]">
+    <div className="bg-timo-gray-300 flex h-4 items-center justify-center rounded-[4px] px-[6.5px]">
       <span className="typo-caption-r-10 text-timo-gray-800 whitespace-nowrap">
         {text}
       </span>
