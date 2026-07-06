44eda5aa79a56e79e7fcbd627a795307514295cc
yumin-kim4757 <rladbals0211@gmail.com>
2026-07-06 18:09:35 +0900

    refactor(ui): TabButton을 콘텐츠 전용 컴포넌트로 변경 (#84)
    - TabButton의 button 태그와 onClick 의존성을 제거했습니다
    
    - Link 내부에서 재사용할 수 있도록 span 기반 콘텐츠 컴포넌트로 변경했습니다
    
    - 클릭 상태를 다루던 인터랙티브 스토리를 제거했습니다

--- packages/timo-design-system/src/components/button/tab-button/TabButton.stories.tsx
+++ packages/timo-design-system/src/components/button/tab-button/TabButton.stories.tsx
@@ -16,10 +16,8 @@ import {
   TodayOffIcon,
   TodayOnIcon,
 } from "@icons";
-import { useState } from "react";
 
 import type { Meta, StoryObj } from "@storybook/react";
-import type { ComponentProps } from "react";
 
 const meta = {
   title: "Components/Button/TabButton",
@@ -33,9 +31,6 @@ const meta = {
     },
     isSelected: {
       control: "boolean",
-    },
-    onClick: {
-      action: "clicked",
     },
   },
   args: {
@@ -138,35 +133,4 @@ export const AllStates: Story = {
       </div>
     </div>
   ),
-};
-
-const InteractiveHomeTab = (args: ComponentProps<typeof TabButton>) => {
-  const [isSelected, setIsSelected] = useState(false);
-
-  return (
-    <TabButton
-      {...args}
-      label="홈"
-      icon={
-        isSelected ? (
-          <HomeOnIcon width={24} height={24} />
-        ) : (
-          <HomeOffIcon width={24} height={24} />
-        )
-      }
-      hoverIcon={<HomeHoverIcon width={24} height={24} />}
-      isSelected={isSelected}
-      onClick={() => {
-        args.onClick?.();
-        setIsSelected((prevIsSelected) => !prevIsSelected);
-      }}
-    />
-  );
-};
-
-export const Interactive: Story = {
-  parameters: {
-    controls: { disable: true },
-  },
-  render: (args) => <InteractiveHomeTab {...args} />,
 };
