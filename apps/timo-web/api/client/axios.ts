aac15e35217788fc5279ee071b8055fd14990c72
kimminna <kmina121777@naver.com>
2026-07-07 00:11:29 +0900

    fix(web): axios baseURL 환경변수 누락 시 에러를 던지도록 수정 (#81)
    - NEXT_PUBLIC_API_BASE_URL 환경변수가 설정되지 않은 경우 즉시 에러를 던지도록 했습니다

--- apps/timo-web/api/client/axios.ts
+++ apps/timo-web/api/client/axios.ts
@@ -2,8 +2,12 @@ import axios from "axios";
 
 import { parseApiError } from "@/api/error/api-error";
 
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+if (!baseURL)
+  throw new Error("NEXT_PUBLIC_API_BASE_URL이 설정되지 않았습니다.");
+
 export const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL,
 });
 
 instance.interceptors.response.use(
