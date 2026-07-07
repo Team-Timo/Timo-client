98cef32784919dd89d385cf9b877d4b60333496e
jjangminii <wjdals030328@catholic.ac.kr>
2026-07-07 23:41:39 +0900

    feat(web): focus 페이지에 타이머 영역을 구현한다 (#109)
    - Figma 디자인을 기반으로 focus 페이지에 타이머(재생/일시정지/시간추가) 영역을 구현했습니다

--- apps/timo-web/app/[locale]/(main)/focus/page.tsx
+++ apps/timo-web/app/[locale]/(main)/focus/page.tsx
@@ -1,3 +1,9 @@
+import { FocusTimerContainer } from "@/app/[locale]/(main)/focus/_containers/FocusTimerContainer";
+
 export default function FocusPage() {
-  return <></>;
+  return (
+    <section className="border-timo-gray-500 flex h-full items-center justify-end border-l bg-white">
+      <FocusTimerContainer />
+    </section>
+  );
 }
