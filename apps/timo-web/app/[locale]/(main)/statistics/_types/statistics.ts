9b8a12b5b08e6f9c2bdbaa5c8884d7683f9c5fdf
yumin-kim4757 <rladbals0211@gmail.com>
2026-07-08 23:52:53 +0900

    feat(web): 통계 사이드 패널 타입 추가 (#123)

---
+++ apps/timo-web/app/[locale]/(main)/statistics/_types/statistics.ts
@@ -0,0 +1,27 @@
+export interface StatisticsMonthSummary {
+  totalRecordMinutes: number;
+  activeDayCount: number;
+  averageRecordedMinutes: number;
+  completedTodoCount: number;
+  totalTodoCount: number;
+}
+
+export interface StatisticsDayDetail {
+  date: string;
+  totalRecordMinutes: number;
+  todos: StatisticsTodoRecord[];
+}
+
+export interface StatisticsTodoRecord {
+  todoId: number;
+  title: string;
+  actualTimeMinutes: number;
+  estimatedTimeMinutes: number;
+  tag: StatisticsTodoTag | null;
+}
+
+export interface StatisticsTodoTag {
+  tagId: number;
+  name: string;
+  color: string;
+}
