3c70014afb1cb058d889369d12a71f52b7a7123e
Lee Hye Won <thankyou@sookmyung.ac.kr>
2026-07-10 16:08:58 +0900

    refactor(web): OnboardingGoogleButtonProps 인터페이스 export 제거 (#118)
    - OnboardingGoogleButtonProps 인터페이스의 불필요한 export 키워드를 제거했습니다

--- apps/timo-web/app/[locale]/onboarding/_components/OnboardingGoogleButton.tsx
+++ apps/timo-web/app/[locale]/onboarding/_components/OnboardingGoogleButton.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 type OnboardingGoogleButtonVariant = "login" | "connectCalendar";
 
-export interface OnboardingGoogleButtonProps {
+interface OnboardingGoogleButtonProps {
   variant: OnboardingGoogleButtonVariant;
   label: string;
   isSelected?: boolean;
