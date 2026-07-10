999c27c7da37e131df59a9d24746a60412add5b4
yumin-kim4757 <rladbals0211@gmail.com>
2026-07-10 22:38:23 +0900

    refactor(web): 통계 페이지 상태를 컨테이너로 이동 (#129)

--- apps/timo-web/app/[locale]/(main)/statistics/page.tsx
+++ apps/timo-web/app/[locale]/(main)/statistics/page.tsx
@@ -1,24 +1,5 @@
-"use client";
-
-import { useState } from "react";
-
-import { StatisticsCalendar } from "@/app/[locale]/(main)/statistics/_components/StatisticsCalendar";
-import { StatisticsHeaderContainer } from "@/app/[locale]/(main)/statistics/_containers/StatisticsHeaderContainer";
-import { MOCK_STATISTICS_CALENDAR } from "@/app/[locale]/(main)/statistics/_mocks/statisticsCalendar";
+import { StatisticsPageContainer } from "@/app/[locale]/(main)/statistics/_containers/StatisticsPageContainer";
 
 export default function StatisticsPage() {
-  const [currentMonth, setCurrentMonth] = useState(() => new Date());
-
-  return (
-    <>
-      <StatisticsHeaderContainer
-        currentMonth={currentMonth}
-        onChangeMonth={setCurrentMonth}
-      />
-      <StatisticsCalendar
-        currentMonth={currentMonth}
-        calendarData={MOCK_STATISTICS_CALENDAR}
-      />
-    </>
-  );
+  return <StatisticsPageContainer />;
 }
