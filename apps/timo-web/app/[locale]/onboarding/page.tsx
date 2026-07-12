c2618637b323c6c657a5ae7ff4f520d2b6d01ef3
Lee Hye Won <thankyou@sookmyung.ac.kr>
2026-07-13 00:34:27 +0900

    feat(web): 온보딩 페이지 인증 가드 추가 (#147)
    - AuthGuardProvider로 감싸 비로그인 시 로그인 페이지로 리다이렉트했습니다

--- apps/timo-web/app/[locale]/onboarding/page.tsx
+++ apps/timo-web/app/[locale]/onboarding/page.tsx
@@ -1,5 +1,10 @@
 import { OnboardingFunnelContainer } from "@/app/[locale]/onboarding/_containers/OnboardingFunnelContainer";
+import { AuthGuardProvider } from "@/providers/auth/AuthGuardProvider";
 
 export default function OnboardingPage() {
-  return <OnboardingFunnelContainer />;
+  return (
+    <AuthGuardProvider>
+      <OnboardingFunnelContainer />
+    </AuthGuardProvider>
+  );
 }
