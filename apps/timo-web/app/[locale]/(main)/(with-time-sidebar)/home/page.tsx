1aa05486ddc72438d481d5d518cd886493e2bb02
Lee Hye Won <thankyou@sookmyung.ac.kr>
2026-07-13 01:17:32 +0900

    fix(web): CI 빌드 에러 수정 - 홈 섹션 에러 폴백 제거 (#147)
    - Server Component에서 Client Component로 함수 prop 전달 불가 제약으로 SectionErrorFallback을 제거했습니다
    - AsyncBoundary에서 errorFallback을 제거하고 기본 Suspense 동작으로 되돌렸습니다

--- apps/timo-web/app/[locale]/(main)/(with-time-sidebar)/home/page.tsx
+++ apps/timo-web/app/[locale]/(main)/(with-time-sidebar)/home/page.tsx
@@ -1,20 +1,15 @@
 import { HomeHeaderContainer } from "@/app/[locale]/(main)/(with-time-sidebar)/home/_containers/HomeHeaderContainer";
 import { HomeTodoContainer } from "@/app/[locale]/(main)/(with-time-sidebar)/home/_containers/HomeTodoContainer";
 import { AsyncBoundary } from "@/components/boundary/AsyncBoundary";
-import { SectionErrorFallback } from "@/components/boundary/SectionErrorFallback";
 
 export default function HomePage() {
   return (
     <div className="flex h-full flex-col">
-      <AsyncBoundary
-        errorFallback={(_, reset) => <SectionErrorFallback reset={reset} />}
-      >
+      <AsyncBoundary>
         <HomeHeaderContainer />
       </AsyncBoundary>
       <section className="min-h-0 flex-1 px-5 pt-2.5">
-        <AsyncBoundary
-          errorFallback={(_, reset) => <SectionErrorFallback reset={reset} />}
-        >
+        <AsyncBoundary>
           <HomeTodoContainer />
         </AsyncBoundary>
       </section>
