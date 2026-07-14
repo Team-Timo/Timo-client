f546d00a9a55b171b4f8aaa5e97d7557a21da7d9
Lee Hye Won <thankyou@sookmyung.ac.kr>
2026-07-14 20:49:31 +0900

    feat(web): today 투두 카드 클릭 시 상세 모달 연결 (#180)
    - 투두 카드를 DetailTodoModalContainer로 감싸 클릭 시 상세 모달이 열리도록 연결했습니다

--- apps/timo-web/app/[locale]/(main)/(with-time-sidebar)/today/_containers/TodayTodoListContainer.tsx
+++ apps/timo-web/app/[locale]/(main)/(with-time-sidebar)/today/_containers/TodayTodoListContainer.tsx
@@ -8,6 +8,7 @@ import { TodayDateHeaderContainer } from "@/app/[locale]/(main)/(with-time-sideb
 import { TodayTodoCardContainer } from "@/app/[locale]/(main)/(with-time-sidebar)/today/_containers/TodayTodoCardContainer";
 import { useTodayTodoList } from "@/app/[locale]/(main)/(with-time-sidebar)/today/_hooks/useTodayTodoList";
 import { useToday } from "@/app/[locale]/(main)/(with-time-sidebar)/today/_queries/use-today";
+import { DetailTodoModalContainer } from "@/components/todo-modal/detail/DetailTodoModalContainer";
 import { formatDate } from "@/utils/date/date";
 import { convertDurationToTimeText } from "@/utils/duration/convert-duration-to-time-text";
 
@@ -24,8 +25,14 @@ const renderTodoIcon = (icon: string | undefined) => {
 
 export const TodayTodoListContainer = () => {
   const { data } = useToday();
-  const { todos, runningTodoId, handlePlay, handleCheck, handleSubTodoCheck } =
-    useTodayTodoList(data.todos);
+  const {
+    todos,
+    runningTodoId,
+    handlePlay,
+    handleCheck,
+    handleDelete,
+    handleSubTodoCheck,
+  } = useTodayTodoList(data.todos);
 
   const completedCount = todos.filter((todo) => todo.completed).length;
 
@@ -37,30 +44,41 @@ export const TodayTodoListContainer = () => {
       />
       <div className="flex min-h-0 flex-1 flex-col gap-2 overflow-y-auto pr-1 pb-4">
         {todos.map((todo) => (
-          <TodayTodoCardContainer
+          <DetailTodoModalContainer
             key={todo.todoId}
-            title={todo.title}
-            isDone={todo.completed}
-            icon={renderTodoIcon(todo.icon)}
-            timerStatus={runningTodoId === todo.todoId ? "RUNNING" : "STOPPED"}
-            toolbar={{
-              date: formatDate(todo.date),
-              dateValue: new Date(todo.date),
-              time: convertDurationToTimeText(todo.durationSeconds),
-              priority: todo.priority,
-              tag: todo.tag?.name,
-              hasMemo: todo.hasMemo,
-              hasRepeat: todo.isRepeated,
-            }}
-            subTodos={todo.subtasks.map((s) => ({
-              id: s.subtaskId,
-              text: s.content,
-              isDone: s.completed,
-            }))}
-            onPlay={() => handlePlay(todo.todoId)}
-            onCheck={() => handleCheck(todo.todoId)}
-            onSubTodoCheck={(id) => handleSubTodoCheck(todo.todoId, id)}
-          />
+            todo={todo}
+            onTogglePlay={() => handlePlay(todo.todoId)}
+            onDelete={() => handleDelete(todo.todoId)}
+          >
+            {(openDetailTodoModal) => (
+              <TodayTodoCardContainer
+                title={todo.title}
+                isDone={todo.completed}
+                icon={renderTodoIcon(todo.icon)}
+                timerStatus={
+                  runningTodoId === todo.todoId ? "RUNNING" : "STOPPED"
+                }
+                toolbar={{
+                  date: formatDate(todo.date),
+                  dateValue: new Date(todo.date),
+                  time: convertDurationToTimeText(todo.durationSeconds),
+                  priority: todo.priority,
+                  tag: todo.tag?.name,
+                  hasMemo: todo.hasMemo,
+                  hasRepeat: todo.isRepeated,
+                }}
+                subTodos={todo.subtasks.map((s) => ({
+                  id: s.subtaskId,
+                  text: s.content,
+                  isDone: s.completed,
+                }))}
+                onCardClick={openDetailTodoModal}
+                onPlay={() => handlePlay(todo.todoId)}
+                onCheck={() => handleCheck(todo.todoId)}
+                onSubTodoCheck={(id) => handleSubTodoCheck(todo.todoId, id)}
+              />
+            )}
+          </DetailTodoModalContainer>
         ))}
       </div>
     </div>
